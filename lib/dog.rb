7ed0d7de776bbf76fa88e0e6104b48a4240f72a5
Aiden <aiden.marques94@gmail.com>
2020-11-25 10:22:52 -0500

    commit tree 43a21be08f7c62e50984ac77a3f541c079edec2d

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+    def initialize
+        @name
+        @breed
+    end 
+
+    def name
+        @name
+    end 
+
+    def breed
+        @breed
+    end 
+
+    def breed=(breed)
+        @breed=breed
+    end 
+
+    def name=(new_name)
+        @name=new_name
+    end
+        
+end 
